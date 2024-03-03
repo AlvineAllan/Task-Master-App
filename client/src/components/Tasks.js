330527062554d56eee55556c053fe92191194e9e
Joses Syanwa <josesyanwa@gmail.com>
2024-03-03 13:59:39 +0300

    Add Tasks

---
+++ client/src/components/Tasks.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Sidebar from './Sidebar';
+import Dashboard from './Dashboard';
+import MyTask from './MyTask';
+import './Task.css';
+import Project from './Project';
+import Calendar from './Calendar';
+
+const Task = () => {
+  const [currentPage, setCurrentPage] = useState('dashboard');
+
+  const handleSidebarItemClick = (page) => {
+    setCurrentPage(page);
+  };
+
+  const getCurrentDate = () => {
+    const currentDate = new Date();
+    const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      timeZone: 'Africa/Nairobi'
+    };
+    return currentDate.toLocaleString('en-US', options);
+  };
+
+  return (
+    <div className="container">
+      <Sidebar onItemClick={handleSidebarItemClick} />
+      <div className="main-content">
+        <h2>Welcome to TaskToDo</h2>
+        <p className="current-date">{getCurrentDate()}</p>
+
+        {/* Rendering components based on currentPage */}
+        {currentPage === 'dashboard' && <Dashboard />}
+        {currentPage === 'tasks' && <MyTask />} {/* Render MyTask component */}
+        {currentPage === 'project' && <Project />}
+        {currentPage === 'calendar' && <Calendar />}
+      </div>
+    </div>
+  );
+};
+
+export default Task;
