e13f6cecc5af0cf8ecb3c53f5d6b4fa3a9ec7372
Moreen <moreencharles@gmail.com>
2024-03-05 01:52:51 +0300

    Added Sidebar

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Sidebar = ({ onItemClick }) => {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>
+          <button onClick={() => onItemClick('dashboard')}>Dashboard</button>
+        </li>
+        <li>
+          <button onClick={() => onItemClick('tasks')}>MyTask</button>
+        </li>
+        <li>
+          <button onClick={() => onItemClick('project')}>Project</button>
+        </li>
+        <li>
+          <button onClick={() => onItemClick('calendar')}>Calendar</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
